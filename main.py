327a6672b9dacdf4ab386dc6a8147fdd4e0468a4
Racer_0815 <96726936+Racer0815@users.noreply.github.com>
2021-12-29 20:11:39 +0100

    updated main.py and added reactrole.json

--- main.py
+++ main.py
@@ -1,23 +1,69 @@
 import discord
+import json
 from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!",intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print("Bot is logged in.")
+    print(f"logged in as {client.user}")
     channel = client.get_channel(922861978484097056)
-    await channel.send('Bot erfolgreich eingeloggt')
+    await channel.send(f'Bot erfolgreich eingeloggt als {client.user}')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='diesem Server'))
-    await channel.send("hier reagieren")
 
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.member.bot:
+        pass
 
-
-async def on_reaction_add(self, reaction, user):
-    test = discord.utils.get(user.guild.roles, name="1")
-    if str(reaction.emoji) == ":✅":
-        await user.add_roles(test)
     else:
-        print("error")
+
+        with open('reactrole.json') as react_file:
+
+            data = json.load(react_file)
+            for x in data:
+                if x['emoji'] == payload.emoji.name and x['message_id'] == payload.message_id:
+                    role = discord.utils.get(client.get_guild(payload.guild_id).roles, id=x['role_id'])
+
+                await payload.member.add_roles(role)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+
+        with open('reactrole.json') as react_file:
+
+            data = json.load(react_file)
+            for x in data:
+                if x['emoji'] == payload.emoji.name and x['message_id'] == payload.message_id:
+                    role = discord.utils.get(client.get_guild(payload.guild_id).roles, id=x['role_id'])
+
+                await client.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
+
+
+@client.command()
+async def reactrole(ctx, emoji, role: discord.Role,*,message):
+
+    emb = discord.Embed(description=message)
+    msg = await ctx.channel.send(embed=emb)
+    await msg.add_reaction(emoji)
+
+    with open('reactrole.json') as json_file:
+        data = json.load(json_file)
+
+        new_react_role = {
+            'role_name':role.name,
+            'role_id':role.id,
+            'emoji':emoji,
+            'message_id':msg.id
+        }
+
+        data.append(new_react_role)
+    with open('reactrole.json','w') as j:
+        json.dump(data,j,indent=4)
+
+
+
+
+
 
 client.run("token")
